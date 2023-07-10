a3a952553e09a20246f5626b82ff76b756c4fcbf
Aleksandr <53820336+Ounor@users.noreply.github.com>
2023-07-10 16:42:10 +0500

    Update main.py

--- less15/main.py
+++ less15/main.py
@@ -5,11 +5,7 @@ class Transport:
         self.max_speed = max_speed
         self.mileage = mileage
 
-class Autobus(Transport):
-    def __init__(self, name, max_speed, mileage):
-        super().__init__(name, max_speed, mileage)
-
-autobus = Autobus("ICARUS", 180, 12)
+autobus = Transport("ICARUS", 180, 12)
 print(f"Название автомобиля: {autobus.name} Скорость: {autobus.max_speed} Пробег: {autobus.mileage}")
 
 
